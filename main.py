23aee0339e30494b1c23d50b14088f7367363b60
Vlasig <vlasig274@gmail.com>
2024-06-17 19:12:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+a = str('Привет, мир!')
+print(len(a))
+
+first = int(25)
+second = int(24)
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+first = int(11)
+second = int(19)
+third = int(15)
+mean = (first + second + third) / 3
+print(int(mean))
+
+first_string = str('Вторник')
+second_string = str('Понедельник')
+print(second_string + ", " + first_string)
+
+a = int(2)
+b = int(5)
+c = int(7)
+f = (a*b)+(b*c)
+print((f**3)/2)
